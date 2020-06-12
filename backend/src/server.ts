adb1d38f4f1fc0dbd00a3de5213dc0e4bd1fcfa3
Guilherme Yabu <guilhermeyabu@MacBook-Pro-de-Guilherme.local>
2020-06-12 17:30:25 -0300

    create base server structure and database connection

---
+++ backend/src/server.ts
@@ -0,0 +1,25 @@
+import express, { response } from 'express'; 
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+/**
+ * Rota: Endereço completo da requisição
+ * 
+ * Recurso: Qual entidade estamos acessando do sistema
+ * 
+ * GET: Buscar informações
+ * POST: Criar informações
+ * PUT: Atualizar informações
+ * DELETE: Deletar informações
+ * 
+ * Request Param: Parâmetros que vem na própria rota que identificam um recurso: /users/1
+ * Query Param: Parâmetros que vem na rota para filtros, paginação
+ * Request Body: Parâmetros para criar/atualizar infos.
+ */
+
+app.use(routes);
+
+app.listen(3333);
